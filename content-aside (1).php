cf4e48aa4977196f9ee3643b2b81f97fb6d830e0
Barry <35274790+superjudge13@users.noreply.github.com>
2019-05-24 11:44:42 +0100

    Delete content-aside (1).php
    duplicate file

--- content-aside (1).php
+++
@@ -1,6 +0,0 @@
-<h2><?php the_title(); ?></h2>
-<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?></div>
-<small>Posted on: <?php the_time('F j, Y'); ?> in <?php the_category(); ?></small>
-<p><?php the_content(); ?></p>
-	
-<hr>
